679ef8d59a886b4e55bab65f09385a066d095a4b
RopFoo <ropfoo@gmail.com>
2020-08-30 18:12:08 +0200

    reformatting

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,7 +1,6 @@
 import '../scss/main.scss';
-import { date } from './date'
-const dateDiv = document.getElementById('datepicker')
+import { date } from './date';
+const dateDiv = document.getElementById('datepicker');
 
-const today = new Date()
-console.log(today)
-date()
+const today = new Date();
+date(today.getMonth(), today.getFullYear(), { startYear: 1930, endYear: 2020 });
--- src/ts/date.ts
+++ src/ts/date.ts
@@ -1,79 +1,126 @@
-import { months, month } from './months'
+import { months, month } from './months';
 
-let month: month
+let currentDate: any;
+
+let day: number;
+let month: month;
+let year: number;
 
 type props = {
-    monthNum?: number,
-    yearNum?: number
-}
+  monthNum?: number;
+  yearNum?: number;
+};
 
-export const date = (monthNum = 5) => {
-    month = months[monthNum]
-    generateDateDiv()
-}
+type yearRange = {
+  startYear: number;
+  endYear: number;
+};
 
+export const date = (
+  currentMonthId: number,
+  currentYear: number,
+  yearRange: yearRange
+) => {
+  month = months[currentMonthId];
+  year = currentYear;
+  generateDateDiv(yearRange);
+};
 
+const updateDate = () => ({
+  date: new Date(year, month.id, 1),
+});
+
+const getDaysInMonth = (month: number, year: number) => {
+  return new Date(year, month, 0).getDate();
+};
 
 const switchMonth = (monthDiv: any, weekDiv: any, direction: string) => {
-    let currentMonthNum = month.id
-    weekDiv.innerHTML = ''
-    if (direction === 'next') {
-        currentMonthNum++
-        month = months[currentMonthNum]
-        monthDiv.innerHTML = month.nameDE
-    } else if (direction === 'prev') {
-        currentMonthNum--
-        month = months[currentMonthNum]
-        monthDiv.innerHTML = month.nameDE
-    }
+  let currentMonthNum = month.id;
+  weekDiv.innerHTML = '';
+  if (direction === 'next') {
+    currentMonthNum++;
+    currentMonthNum > 11 && (currentMonthNum = 0);
+    month = months[currentMonthNum];
+    monthDiv.innerHTML = month.nameDE;
+  } else if (direction === 'prev') {
+    currentMonthNum--;
+    currentMonthNum < 0 && (currentMonthNum = 11);
+    month = months[currentMonthNum];
+    monthDiv.innerHTML = month.nameDE;
+  }
 
-    month = months[currentMonthNum]
-    weekDiv.append(generateWeekDiv())
-    console.log(month)
-}
+  month = months[currentMonthNum];
+  weekDiv.append(generateWeekDiv());
+  console.log(month);
+  currentDate = updateDate();
+  console.log(currentDate);
+};
 
-const generateDateDiv = () => {
+const generateDateDiv = (yearRange: yearRange) => {
+  const weekDiv = generateWeekDiv();
 
-    const weekDiv = generateWeekDiv()
+  // Month
+  const monthDiv = document.createElement('div');
+  const monthContent = document.createTextNode(month.nameDE);
+  monthDiv.append(monthContent);
 
-    // Month
-    const monthDiv = document.createElement('div')
-    const monthContent = document.createTextNode(month.nameDE)
-    monthDiv.append(monthContent)
+  // Next Month
+  const nextMonthBtn = document.createElement('button');
+  const nextMonthBtnContent = document.createTextNode('>');
+  nextMonthBtn.append(nextMonthBtnContent);
+  nextMonthBtn.addEventListener('click', () => {
+    switchMonth(monthDiv, weekDiv, 'next');
+  });
 
-    // Next Month
-    const nextMonthBtn = document.createElement('button')
-    const nextMonthBtnContent = document.createTextNode('>')
-    nextMonthBtn.append(nextMonthBtnContent)
-    nextMonthBtn.addEventListener('click', () => { switchMonth(monthDiv, weekDiv, 'next') })
+  // Prev Month
+  const prevMonthBtn = document.createElement('button');
+  const prevMonthBtnContent = document.createTextNode('<');
+  prevMonthBtn.append(prevMonthBtnContent);
+  prevMonthBtn.addEventListener('click', () => {
+    switchMonth(monthDiv, weekDiv, 'prev');
+  });
 
-    // Prev Month
-    const prevMonthBtn = document.createElement('button')
-    const prevMonthBtnContent = document.createTextNode('<')
-    prevMonthBtn.append(prevMonthBtnContent)
-    prevMonthBtn.addEventListener('click', () => { switchMonth(monthDiv, weekDiv, 'prev') })
+  // Year
+  const yearDiv = document.createElement('div');
+  const yearDropdown = document.createElement('select');
+  for (
+    let selectedYear = yearRange.startYear;
+    selectedYear <= yearRange.endYear;
+    selectedYear++
+  ) {
+    const yearOption = document.createElement('option');
+    yearOption.value = selectedYear.toString();
+    yearOption.innerHTML = selectedYear.toString();
+    yearDropdown.append(yearOption);
+  }
+  yearDropdown.value = yearRange.endYear.toString();
+  yearDropdown.addEventListener('change', (e) => {
+    weekDiv.innerHTML = '';
+    const element = e.currentTarget as HTMLInputElement;
+    year = parseInt(element.value);
+    weekDiv.append(generateWeekDiv());
+    currentDate = updateDate();
+    console.log(currentDate);
+  });
+  yearDiv.append(yearDropdown);
 
+  const dateDiv = document.createElement('div');
+  dateDiv.append(yearDiv, prevMonthBtn, monthDiv, nextMonthBtn, weekDiv);
 
-
-    const dateDiv = document.createElement('div')
-    dateDiv.append(prevMonthBtn, monthDiv, nextMonthBtn, weekDiv)
-
-    const target = document.getElementById('datepicker')
-    document.body.insertBefore(dateDiv, target)
-
-}
+  const target = document.getElementById('datepicker');
+  document.body.insertBefore(dateDiv, target);
+};
 
 const generateWeekDiv = () => {
-    const weekDiv = document.createElement('div')
+  const weekDiv = document.createElement('div');
 
-    for (let day = 1; day <= month.days; day++) {
-        // Day
-        const dayDiv = document.createElement('div')
-        const dayContent = document.createTextNode(day.toString())
-        dayDiv.append(dayContent)
-        weekDiv.append(dayDiv)
-    }
+  for (let day = 1; day <= getDaysInMonth(month.number, year); day++) {
+    // Day
+    const dayDiv = document.createElement('div');
+    const dayContent = document.createTextNode(day.toString());
+    dayDiv.append(dayContent);
+    weekDiv.append(dayDiv);
+  }
 
-    return weekDiv
-
-}
+  return weekDiv;
+};
