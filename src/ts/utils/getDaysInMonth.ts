0b79af5ac6109bda9e6800db9e1282404ab57daa
RopFoo <ropfoo@gmail.com>
2020-09-24 14:02:34 +0200

    added more jsdoc

--- src/ts/utils/getDaysInMonth.ts
+++ src/ts/utils/getDaysInMonth.ts
@@ -2,8 +2,9 @@
  * Returns the amount of days in a month of a year
  * @param {number} month
  * @param {number} year
+ * @returns {number} - amount of days in month as number
  */
-const getDaysInMonth = (month: number, year: number) => {
+const getDaysInMonth = (month: number, year: number): number => {
   return new Date(year, month, 0).getDate();
 };
 
